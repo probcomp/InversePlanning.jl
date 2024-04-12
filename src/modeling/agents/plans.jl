46bea9733adcb370dfddd50869009ea48d027dd4
Xuan <tanqazx@gmail.com>
2024-04-11 20:38:51 -0400

    Support periodic or lazy refinement of policies, refinement of plans.

--- src/modeling/agents/plans.jl
+++ src/modeling/agents/plans.jl
@@ -41,6 +41,24 @@ has_action(sol::OrderedSolution, t::Int, state::State) =
 has_action(sol::PathSearchSolution, t::Int, state::State) =
     !ismissing(SymbolicPlanners.get_action(sol, state))
 
+"Returns whether there are cached action value for step `t` at a `belief_state`."
+has_cached_action(plan_state::PlanState, t::Int, belief_state) =
+    has_cached_action(plan_state.sol, t - plan_state.init_step + 1, belief_state)
+has_cached_action(sol::Solution, t::Int, state::State) =
+    has_action(sol, t, state)
+has_cached_action(sol::PolicySolution, t::Int, state::State) =
+    SymbolicPlanners.has_cached_action_values(sol, state)
+
+"Returns whether the solution has a search node for `belief_state`."
+has_search_node(plan_state::PlanState, belief_state) = 
+    has_search_node(plan_state.sol, belief_state)
+has_search_node(sol::Solution, state::State) =
+    false
+has_search_node(sol::PathSearchSolution, state::State) =
+    SymbolicPlanners.is_expanded(state, sol)
+has_search_node(sol::ReusableTreePolicy, state::State) =
+    SymbolicPlanners.is_expanded(state, sol.search_sol)
+
 """
     PlanConfig
 
@@ -151,6 +169,8 @@ end
         domain::Domain, planner::Planner;
         plan_at_init::Bool = false,
         prob_replan::Real=0.1,
+        prob_refine::Real=0.0,
+        replan_period::Int=1,
         rand_budget::Bool = true,
         budget_var::Symbol = default_budget_var(planner),
         budget_dist::Distribution = shifted_neg_binom,
@@ -164,13 +184,15 @@ function ReplanConfig(
     domain::Domain, planner::Planner;
     plan_at_init::Bool = false,
     prob_replan::Real = 0.1,
+    prob_refine::Real = 0.0,
+    replan_period::Int = 1,
     rand_budget::Bool = true,
     budget_var::Symbol = default_budget_var(planner),
     budget_dist::Distribution = shifted_neg_binom,
     budget_dist_args::Tuple = (2, 0.05, 1)
 )
-    step_args = (domain, planner, prob_replan, rand_budget,
-                 budget_var, budget_dist, budget_dist_args)
+    step_args = (domain, planner, prob_replan, prob_refine, replan_period,
+                 rand_budget, budget_var, budget_dist, budget_dist_args)
     if plan_at_init
         init = step_plan_init
         init_args = (replan_step, step_args)
@@ -186,36 +208,57 @@ default_budget_var(::ForwardPlanner) = :max_nodes
 
 """
     replan_step(t, plan_state, belief_state, goal_state, domain, planner,
-                prob_replan=0.1, budget_var=:max_nodes,
+                prob_replan=0.1, prob_refine=0.0, replan_period=1,
+                rand_budget=true, budget_var=:max_nodes,
                 budget_dist=shifted_neg_binom, budget_dist_args=(2, 0.95, 1))
 
-Replanning step for ordered planners. At each timestep, a decision is made 
-whether to replan, and replanning uses a randomly sampled resource budget.
+Replanning step for fully-ordered planners. After each `replan_period`, a
+decision is made to either replan from scratch or refine the previous search
+solution. If `rand_budget` is true, replanning or refinement is performed up to
+a randomly sampled maximum resource budget.
 """
 @gen function replan_step(
     t::Int, plan_state::PlanState, belief_state::State, goal_state,
     domain::Domain, planner::Planner,
     prob_replan::Real=0.1,
+    prob_refine::Real=0.0,
+    replan_period::Int=1,
     rand_budget::Bool=true,
     budget_var::Symbol=:max_nodes,
     budget_dist::Distribution=shifted_neg_binom,
     budget_dist_args::Tuple=(2, 0.05, 1)
 )   
     spec = convert(Specification, goal_state)
-    # Replan with certainty if action is unplanned or goal changes
-    if (!has_action(plan_state, t, belief_state) ||
+    if (plan_state.sol isa NullSolution ||
         (plan_state.spec !== spec && plan_state.spec != spec))
+        # Replan with certainty if no solution exists or goal changes
         prob_replan = 1.0
+        prob_refine = 0.0
+    elseif !has_action(plan_state, t, belief_state)
+        # Enforce recomputation if action is unplanned
+        prob_recompute = prob_replan + prob_refine
+        if prob_recompute <= 0.0 || !has_search_node(plan_state, belief_state)
+            prob_replan = 1.0
+            prob_refine = 0.0
+        else
+            prob_replan = prob_replan / prob_recompute
+            prob_refine = prob_refine / prob_recompute
+        end
+    elseif t % replan_period != 0
+        # Otherwise only replan/refine periodically
+        prob_refine = 0.0
+        prob_replan = 0.0
     end
-    # Sample whether to replan
-    replan = {:replan} ~ bernoulli(prob_replan)
+    # Sample whether to replan or refine
+    probs = [1-(prob_replan+prob_refine), prob_replan, prob_refine]
+    replan = {:replan} ~ categorical(probs)
     if rand_budget # Sample planning resource budget
         budget = {:budget} ~ budget_dist(budget_dist_args...)
     end
-    # Decide whether to replan
-    if !replan # Return original plan
+    # Decide whether to replan or refine
+    if replan == 1 # Return original plan
         return plan_state
-    else # Otherwise replan from the current belief state
+    elseif replan == 2 # Replan from the current belief state
         if rand_budget # Set new resource budget
             planner = copy(planner)
             setproperty!(planner, budget_var, budget)
@@ -223,6 +266,15 @@ whether to replan, and replanning uses a randomly sampled resource budget.
         # Compute and return new plan
         sol = planner(domain, belief_state, spec)
         return PlanState(t, sol, spec)
+    elseif replan == 3 # Refine existing solution
+        if rand_budget # Set new resource budget
+            planner = copy(planner)
+            setproperty!(planner, budget_var, budget)
+        end
+        # Refine existing solution
+        sol = copy(plan_state.sol)
+        refine!(sol, planner, domain, belief_state, spec)
+        return PlanState(plan_state.init_step, sol, spec)
     end
 end
 
@@ -232,6 +284,8 @@ end
         plan_at_init::Bool = false,
         prob_replan::Real = 0.05,
         prob_refine::Real = 0.2,
+        replan_period::Int = 1,
+        replan_cond::Symbol = :unplanned,
         rand_budget::Bool = true,
         budget_var::Symbol = default_budget_var(planner),
         budget_dist::Distribution = shifted_neg_binom,
@@ -239,21 +293,25 @@ end
     )
 
 Constructs a `PlanConfig` that may stochastically recompute or refine a policy
-at each timestep. If `plan_at_init` is true, then the initial plan is computed
-at timestep zero.
+at regular intervals (controlled by `replan_period`). If `plan_at_init` is true,
+then the initial plan is computed at timestep zero.
 """
 function ReplanPolicyConfig(
     domain::Domain, planner::Planner;
     plan_at_init::Bool = false,
     prob_replan::Real = 0.05,
     prob_refine::Real = 0.2,
+    replan_period::Int = 1,
+    replan_cond::Symbol = :unplanned,
     rand_budget::Bool = true,
     budget_var::Symbol = default_budget_var(planner),
     budget_dist::Distribution = shifted_neg_binom,
     budget_dist_args::Tuple = (2, 0.05, 1)
 )
-    step_args = (domain, planner, prob_replan, prob_refine,
-                 rand_budget, budget_var, budget_dist, budget_dist_args)
+    step_args = (
+        domain, planner, prob_replan, prob_refine, replan_period, replan_cond,
+        rand_budget, budget_var, budget_dist, budget_dist_args
+    )
     if plan_at_init
         init = step_plan_init
         init_args = (policy_step, step_args)
@@ -269,32 +327,48 @@ default_budget_var(::RealTimeHeuristicSearch) = :max_nodes
 
 """
     policy_step(t, plan_state, belief_state, goal_state, domain, planner,
-                prob_replan=0.05, prob_refine=0.2, rand_budget=true,
-                budget_var=:max_depth, budget_dist=shifted_neg_binom,
-                budget_dist_args=(2, 0.95, 1))
+                prob_replan=0.05, prob_refine=0.2, replan_period=1,
+                replan_cond=:unplanned, rand_budget=true, budget_var=:max_depth,
+                budget_dist=shifted_neg_binom, budget_dist_args=(2, 0.95, 1))
 
-Replanning step for policy-based planners. At each timestep, a decision is made 
-whether to refine the existing policy or replan from scratch. If `rand_budget`
-is true, policy computation or refinement is performed up to randomly sampled
-maximum resource budget.
+Replanning step for policy-based planners. After each `replan_period`, a
+decision is made whether to refine the existing policy or replan from scratch.
+If `rand_budget` is true, policy computation or refinement is performed up to
+a randomly sampled maximum resource budget.
 """
 @gen function policy_step(
     t::Int, plan_state::PlanState, belief_state::State, goal_state,
     domain::Domain, planner::Planner,
     prob_replan::Real=0.05,
     prob_refine::Real=0.2,
+    replan_period::Int=1,
+    replan_cond::Symbol=:unplanned,
     rand_budget::Bool=true,
     budget_var::Symbol=:max_depth,
     budget_dist::Distribution=shifted_neg_binom,
     budget_dist_args::Tuple=(2, 0.05, 1)
 )
     spec = convert(Specification, goal_state)
-    # Replan with certainty if action is unplanned or goal changes
-    if (!has_action(plan_state, t, belief_state) ||
-        plan_state.sol isa NullPolicy ||
+    if (plan_state.sol isa NullPolicy || plan_state.sol isa NullSolution ||
         (plan_state.spec !== spec && plan_state.spec != spec))
+        # Replan with certainty if no solution exists or goal changes
         prob_replan = 1.0
         prob_refine = 0.0
+    elseif ((replan_cond == :unplanned && !has_action(plan_state, t, belief_state)) ||
+            (replan_cond == :uncached && !has_cached_action(plan_state, t, belief_state)))
+        # Enforce recomputation if action is unplanned or uncached
+        prob_recompute = prob_replan + prob_refine
+        if prob_recompute <= 0.0
+            prob_replan = 1.0
+            prob_refine = 0.0
+        else
+            prob_replan = prob_replan / prob_recompute
+            prob_refine = prob_refine / prob_recompute
+        end
+    elseif t % replan_period != 0
+        # Otherwise only replan/refine periodically
+        prob_refine = 0.0
+        prob_replan = 0.0
     end
     # Sample whether to replan or refine
     probs = [1-(prob_replan+prob_refine), prob_replan, prob_refine]
